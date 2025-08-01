a7ea66ebd734ecb0f89f2fb2bdca392bec6508e5
unknown <pantho883@gmail.com>
2025-08-02 00:13:16 +0600

    sign in login demo color changed

--- src/pages/Resgister.jsx
+++ src/pages/Resgister.jsx
@@ -9,37 +9,37 @@ const Resgister = () => {
      const [show,setShow]=useState(false)
   return (
     <>
-     <section id='signin' className=' w-full h-screen flex items-center bg-[#5587d1] '>
+     <section id='signin' className=' w-full h-screen flex items-center bg-[#071d3f] '>
         
-            <div className='w-[310px] h-[410px] bg-[#dfd3d3] pt-4 mx-auto mt-[100px] rounded-[5px]'>
+            <div className='w-[310px] h-[450px] bg-[#02021b] pt-4 mx-auto mt-[100px]   rounded-[14px] border-[3px] border-white'>
              
-                <div className="iconbox mx-auto w-[50%] flex items-center justify-center gap-1.5 font-bold text-[20px] mt-[20px]">
+                <div className="iconbox mx-auto w-[50%] flex items-center justify-center text-white gap-1.5 font-bold text-[24px]  mt-[20px]">
                     TeeTalk<RiChatUnreadFill />
                 </div>
-                 <h1 className='text-center text-[24px] font-bold text-lime-800'>Registration</h1>
+                 <h1 className='text-center text-[14px] font-bold text-[#56a3c7]'>Registration</h1>
                 {/* input box starts */}
                 <div className="inputbox flex justify-center items-center py-2 mt-2 flex-col">
                   {/* user name start */}
-                   <input className='w-[250px] p-1 bg-amber-50 rounded-[6px] outline-0 mt-2' type='text' placeholder='User Name' />
+                   <input className='w-[250px] p-1 text-white bg-[#312e2e] rounded-[6px] outline-0 mt-2 placeholder-gray-400' type='text' placeholder='User Name' />
                   {/* user name end */}
                   {/* email start */}
-                <input className='w-[250px] p-1 bg-amber-50 rounded-[6px] outline-0 mt-5' type='email' placeholder='Email Address' />
+                <input className='w-[250px] p-1 bg-[#312e2e] text-white rounded-[6px] outline-0 mt-5 placeholder-gray-400' type='email' placeholder='Email Address' />
                  {/* email end */}
                 {/* password start */}
-                <div className='w-[250px] bg-amber-50 mt-5 p-1 rounded-[6px] flex justify-between'>
+                <div className='w-[250px] bg-[#312e2e]  mt-5 p-1 rounded-[6px] flex justify-between'>
                 <input  
-                className='outline-0' 
+                className='outline-0 placeholder-gray-400 text-white' 
                 placeholder='Password'
                 type={show?"text":"password"}  />
-                <button className=' w-[30px] cursor-pointer p-1 ' onClick={()=>{setShow(!show)}}>{show?<FaEyeSlash/>:<FaEye /> }</button>
+                <button className=' w-[30px] cursor-pointer p-1 ' onClick={()=>{setShow(!show)}}>{show?<FaEyeSlash className='text-white'/>:<FaEye className='text-white'/> }</button>
                 </div>
                   {/* password end */}
                 </div>
                   {/* input box ends */}
                   {/* Button box starts */}
                 <div className="buttonbox flex flex-col">
-                           <Link to='/reg' className='w-[140px] py-2 bg-[#a07d1d] rounded-[4px] text-white h-[30px] mx-auto mt-[20px] flex items-center justify-center' >Create Account</Link>
-                           <Link to={'/'} className='w-[140px] bg-[#186b1c] py-2 rounded-[4px] text-white h-[30px] mx-auto mt-[20px]   flex items-center justify-center'>SingIn page</Link>
+                           <Link to='/reg' className='w-[140px] py-2 bg-[#a07d1d] rounded-[4px] text-white h-[40px] mx-auto mt-[20px] font-semibold  flex items-center justify-center' >Create Account</Link>
+                           <Link to={'/'} className='w-[140px] bg-[#144c7a]   rounded-[4px] text-white h-[40px] mx-auto mt-[20px] font-semibold  flex items-center justify-center'>SingIn page</Link>
              
                          </div>
                  {/* Button box ends */}
--- src/pages/Singin.jsx
+++ src/pages/Singin.jsx
@@ -8,29 +8,29 @@ const Singin = () => {
     const [show,setShow]=useState(false)
   return (
     
-    <section id='signin' className=' w-full h-screen  flex items-center bg-[#189196] '>
+    <section id='signin' className=' w-full h-screen  flex items-center bg-[#071d3f] '>
     
-        <div className='w-[310px] h-[380px] bg-[#dfd3d3] pt-4 mx-auto mt-[100px] rounded-[5px]'>
-            <div className="iconbox mx-auto w-[50%] flex items-center justify-center gap-1.5 font-bold text-[20px] mt-[50px]">
+        <div className='w-[310px] h-[380px] bg-[#02021b] pt-4 mx-auto mt-[100px] rounded-[14px] border-[3px] border-white'>
+            <div className="iconbox mx-auto w-[50%] flex items-center justify-center gap-1.5 font-bold text-[27px] mt-[30px] text-white">
                 TeeTalk<RiChatUnreadFill />
             </div>
             {/* input box starts */}
             <div className="inputbox flex justify-center items-center py-2 mt-2 flex-col">
-            <input className='w-[250px] p-1 bg-amber-50 rounded-[6px] outline-0 mt-2' type="text" placeholder='Email Address' />
+            <input className='w-[250px] p-1 bg-[#312e2e] rounded-[6px] outline-0 mt-2 text-white placeholder-gray-400' type="text" placeholder='Email Address' />
             {/* password start */}
-            <div className='w-[250px] bg-amber-50 mt-5 p-1 rounded-[6px] flex justify-between'>
+            <div className='w-[250px] bg-[#312e2e] mt-5 p-1 rounded-[6px] flex justify-between'>
             <input  
-            className='outline-0' 
+            className='outline-0 placeholder-gray-400 text-white' 
             type={show?"text":"password"} placeholder='Password'/>
-  <button className=' w-[30px] cursor-pointer p-1 ' onClick={()=>{setShow(!show)}}>{show?<FaEyeSlash />:<FaEye />}</button>
+  <button className=' w-[30px] cursor-pointer p-1 ' onClick={()=>{setShow(!show)}}>{show?<FaEyeSlash className='text-white'/>:<FaEye className='text-white'/>}</button>
             </div>
               {/* password end */}
             </div>
               {/* input box ends */}
               {/* Button box starts */}
             <div className="buttonbox flex flex-col">
-              <Link className='w-[100px] bg-[#186b1c] py-2 rounded-[4px] text-white h-[30px] mx-auto mt-[20px]   flex items-center justify-center'>SingIn</Link>
-              <Link to='/reg' className='w-[100px] py-2 bg-[#1d81a0] rounded-[4px] text-white h-[30px] mx-auto mt-[20px] flex items-center justify-center' >SignUp</Link>
+              <Link className='w-[140px] py-2 bg-[#1c848b] rounded-[4px] text-white h-[40px] mx-auto mt-[20px] font-semibold  flex items-center justify-center'>SingIn</Link>
+              <Link to='/reg' className='w-[140px] bg-[#144c7a]   rounded-[4px] text-white h-[40px] mx-auto mt-[20px] font-semibold  flex items-center justify-center' >SignUp</Link>
 
             </div>
              {/* Button box ends */}
