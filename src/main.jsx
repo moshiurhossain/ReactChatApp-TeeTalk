7e10236cef8897c9e7c4e1aa0cbab2c38dc1391c
unknown <pantho883@gmail.com>
2025-08-15 17:59:25 +0600

    redux setup started

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import myStore from './store.js'
+import { Provider } from 'react-redux'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <>
+    <Provider store={myStore}>
     <App />
+     </Provider>
+    </>
+   
   </StrictMode>,
 )
