98c8ef73b4835a305680dbaef4443ddd10c85fc6
unknown <pantho883@gmail.com>
2025-09-11 00:16:55 +0600

    unblock btn bug fix

--- src/pages/BlockedUser.jsx
+++ src/pages/BlockedUser.jsx
@@ -24,15 +24,16 @@ const BlockedUser = () => {
 console.log(blockUser)
 // 
 const handleUnblock =(unblockInfo)=>{
+  console.log(unblockInfo)
      set(push(ref(db, 'chatUser/')), {
        
        senderID: currentUserInfo.uid,
        senderName: currentUserInfo.displayName,
        senderPhoto: currentUserInfo.photoURL,
     
-       adderId: unblockInfo.blockUserId,
-       adderName: unblockInfo.blockUserName,
-       adderAvatar:unblockInfo.blockUserPicture, 
+       adderId: unblockInfo.userInfo.blockUserId,
+       adderName: unblockInfo.userInfo.blockUserName,
+       adderAvatar:unblockInfo.userInfo.blockUserPicture, 
 
       });
       remove(ref(db,'blockUserList/' + unblockInfo.key))
@@ -44,7 +45,7 @@ const handleUnblock =(unblockInfo)=>{
         {
             blockUser.map((item)=>(
        
-      <div className='blockuserCard m-3 flex justify-between items-center w-[400px] py-2 h-fit px-3 rounded-[5px] shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)]'>
+      <div key={item.key} className='blockuserCard m-3 flex justify-between items-center w-[400px] py-2 h-fit px-3 rounded-[5px] shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)]'>
            <div className="flex items-center gap-2">
             <div className="w-10 h-10 rounded-full bg-gray-300 overflow-hidden"><img src={item.userInfo.blockUserPicture} alt="" /></div>
             <div className='h-fit'>
